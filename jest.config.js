8cbbd22e0e34ff1629058b479aeb229a92c1d28e
Vitaliy Kim <omegarekrut@gmail.com>
2024-04-14 13:46:16 +0600

    Deleted comments

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  preset: "ts-jest", // Use the 'ts-jest' preset for handling TypeScript files
-  testEnvironment: "node", // Set the test environment to node
+  preset: "ts-jest", 
+  testEnvironment: "node", 
   transform: {
-    "^.+\\.(t|j)sx?$": "babel-jest", // Use babel-jest to transform files with these extensions
+    "^.+\\.(t|j)sx?$": "babel-jest", 
   },
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1", // Handle path mapping
+    "^(\\.{1,2}/.*)\\.js$": "$1", 
   },
   transformIgnorePatterns: [
-    "node_modules/(?!your-es-modules)", // You can specify node_modules to be ignored except for some
+    "node_modules/(?!your-es-modules)", 
   ],
 };
